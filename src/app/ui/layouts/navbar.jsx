a16341caae9c84bb28c05167830ec4a4dd9d9127
isogharibi2 <gharibi55059@gmail.com>
2025-04-21 18:34:47 +0330

    add reset the navbar styles and elemnts

--- src/app/ui/layouts/navbar.jsx
+++ src/app/ui/layouts/navbar.jsx
@@ -4,101 +4,68 @@ import "./navbar.scss";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function navbar() {
-  const Navigate = useNavigate();
+
+  const loginState = localStorage.getItem('verified');
+
+  const navigate = useNavigate()
 
   const links = [
     {
       id: 0,
+      title: "صفحه اصلی",
+      url: "/",
+    },
+    {
+      id: 1,
       title: "تماس با ما",
       url: "/contact-us",
     },
     {
-      id: 1,
+      id: 2,
       title: "درباره ما",
       url: "/about-us",
     },
     {
-      id: 2,
-      title: "وبلاگ",
-      url: "/blog",
-    },
-    {
       id: 3,
       title: "وبلاگ",
       url: "/blog",
     },
   ];
-
-  const NavbarOpt = [
-    {
-      id: 1,
-      title: "کاپشن",
-    },
-    {
-      id: 2,
-      title: "ژاکت",
-    },
-    {
-      id: 3,
-      title: "کت و شلوار",
-    },
-    {
-      id: 4,
-      title: "شلوار",
-    },
-    {
-      id: 5,
-      title: "تیشرت",
-    },
-    {
-      id: 6,
-      title: "پیراهن",
-    },
-  ];
-
-  function NavigateUser() {
-    Navigate(
-      localStorage.getItem("verified") ? "/profile/user" : "/auth/register"
-    );
-  }
 
   return (
-    <section>
+    <>
       <nav>
-        {links.map((link) => (
-          <ul key={link.id}>
-            <li>
+        <ul>
+          {links.map((link) => (
+            <li key={link.id}>
               <Link to={link.url}> {link.title} </Link>
             </li>
-          </ul>
-        ))}
+          ))}
+        </ul>
 
         <button className="cart">
           سبد خرید
           <div className="indicator">2</div>
         </button>
-        <button
-          style={{ cursor: "pointer" }}
-          className="profile-me"
-          onClick={NavigateUser}
-        >
-          {localStorage.getItem("verified") ? "پروفایل من" : "ثبت نام / ورود"}
+        <button className="profile-me" onClick={() => navigate(loginState ? '/profile/user' : '/auth/register')}>
+          {loginState ? "پروفایل من" : "ثبت نام / ورود"}
         </button>
       </nav>
-      <div className="title">
-        <span className="TitleName">BLACK DARK</span>
-        <ul className="ul">
-          {NavbarOpt.map((Clothes) => (
-            <li key={Clothes.id}>
-              <Link to={Clothes.url}> {Clothes.title} </Link>
-            </li>
-          ))}
-        </ul>
-        <div className="search">
-          <input type="search-box" placeholder="جستجو" />
+      <div className="sub-nav">
+        <div className="search-bar">
           <img src="/public/assets/icons/Vector.svg" alt="" />
+          <input type="text" name="search-all" id="searchTheWholeSite" placeholder="جستجو" />
         </div>
+        <ul>
+          <li> پیراهن </li>
+          <li> تیشرت </li>
+          <li> شلوار </li>
+          <li> کت و شلوار </li>
+          <li> ژاکت </li>
+          <li> کاپشن </li>
+        </ul>
+        <h3>BLACK DARK</h3>
       </div>
-    </section>
+    </>
   );
 }
