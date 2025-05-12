5bbe131f1b92d12f8edd8a55785161630a51177a
isogharibi2 <gharibi55059@gmail.com>
2025-05-12 19:52:49 +0330

    adding the new categories to the api and change the value ni api

--- src/app/pages/profile/admin.jsx
+++ src/app/pages/profile/admin.jsx
@@ -1,18 +1,32 @@
 import React, { useEffect, useState } from "react";
 
 import "./Admin.scss";
-import { QueryClient, useMutation } from "@tanstack/react-query";
+import {
+  QueryClient,
+  useMutation,
+  useQueries,
+  useQuery,
+} from "@tanstack/react-query";
 import axios from "axios";
 import { baseUrl } from "@app/helpers/variables";
 import { useNavigate } from "react-router-dom";
 
 export default function NewProduct() {
-
-    const navigate = useNavigate();
-    const queryClient = new QueryClient();
+  const navigate = useNavigate();
+  const queryClient = new QueryClient();
 
   const [imagePreview, setImagePreview] = useState(null);
   const [imageSrc, setImageSrc] = useState(null);
+  const [categoryInput, setcategoryInput] = useState("");
+
+  //  const categories = [
+  //   "پیراهن",
+  //   "تیشرت",
+  //   "شلوار",
+  //   "کت و شلوار",
+  //   "ژاکت",
+  //   "کاپشن"
+  // ]
 
   const handleImageChange = (event) => {
     const file = event.target.files[0];
@@ -27,43 +41,48 @@ export default function NewProduct() {
     if (!localStorage.getItem("verified")) {
       navigate("/auth/register");
     }
-  })
+  });
 
   const mutationFn = async (event) => {
-
     event.preventDefault();
     const formData = new FormData(event.target);
 
+    if (formData.get("category") != categoryInput) {
+      await axios.post(`${baseUrl}/categories`, {
+        category: formData.get("category"),
+      });
+    }
+
     try {
       const response = await axios.post(`${baseUrl}/specail-offers`, {
-        title: formData.get('title'),
-        enTitle: formData.get('en-title'),
-        brand: formData.get('brand'),
-        sizeSM: formData.get('sizeSM'),
-        sizeS: formData.get('sizeS'),
-        sizeM: formData.get('sizeM'),
-        sizeL: formData.get('sizeL'),
-        sizeXL: formData.get('sizeXL'),
-        colorB: formData.get('colorB'),
-        colorBe: formData.get('colorBe'),
-        colorR: formData.get('colorR'),
-        colorG: formData.get('colorG'),
-        colorP: formData.get('colorP'),
-        originalPrice: formData.get('originalPrice'),
-        offerPrice: formData.get('offerPrice'),
-        percentage: formData.get('percentage'),
+        title: formData.get("title"),
+        enTitle: formData.get("en-title"),
+        brand: formData.get("brand"),
+        sizeSM: formData.get("sizeSM"),
+        sizeS: formData.get("sizeS"),
+        sizeM: formData.get("sizeM"),
+        sizeL: formData.get("sizeL"),
+        sizeXL: formData.get("sizeXL"),
+        colorB: formData.get("colorB"),
+        colorBe: formData.get("colorBe"),
+        colorR: formData.get("colorR"),
+        colorG: formData.get("colorG"),
+        colorP: formData.get("colorP"),
+        originalPrice: formData.get("originalPrice"),
+        offerPrice: formData.get("offerPrice"),
+        percentage: formData.get("percentage"),
+        category: formData.get("category"),
         img: imageSrc,
         img: imageSrc,
-        slug: formData.get('en-title'),
+        slug: formData.get("en-title"),
       });
       return response.data;
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   };
 
   const { mutate, isLoading, isError, error } = useMutation({
-
     mutationFn,
 
     onError: (error) => {
@@ -72,47 +91,119 @@ export default function NewProduct() {
     },
 
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['product-detail'] })
+      queryClient.invalidateQueries({ queryKey: ["product-detail"] });
+    },
+  });
+
+  const queryFn = async () => {
+    try {
+      const { data } = await axios.get(`${baseUrl}/categories`);
+      return data;
+    } catch (err) {
+      console.error(err);
     }
+  };
+
+  const { data: categories } = useQuery({
+    queryKey: ["categories"],
+    queryFn,
   });
 
   return (
     <>
       <form className="newProduct" onSubmit={mutate}>
-        <section className='rightSide'>
+        <section className="rightSide">
           <fieldset>
             <legend> اطلاعات کلی محصول </legend>
-            <input className="newPrInput" name='title' type="text" placeholder='عنوان محصول شما' />
-            <input className="newPrInput" name='en-title' type="text" placeholder='عنوان انگیلیسی محصول شما' />
-            <input className="newPrInput" name='brand' type="text" placeholder='برند مصحول شما' />
+            <input
+              className="newPrInput"
+              name="title"
+              type="text"
+              placeholder="عنوان محصول شما"
+            />
+            <input
+              className="newPrInput"
+              name="en-title"
+              type="text"
+              placeholder="عنوان انگیلیسی محصول شما"
+            />
+            <input
+              className="newPrInput"
+              name="brand"
+              type="text"
+              placeholder="برند مصحول شما"
+            />
+            <input
+              className="newPrInput"
+              name="category"
+              type="text"
+              placeholder="اضافه کردن دسته بندی"
+              value={categoryInput}
+              onChange={(e) => setcategoryInput(e.target.value)}
+            />
+
+            <select name="" id="">
+              {categoryInput !== "" ? <option value={category}>{category}</option>
+                : categories?.map((category, index) => (
+                    <option key={index} value={category.category}>
+                      {" "}
+                      {category.category}{" "}
+                    </option>
+                  ))}
+            </select>
           </fieldset>
           <fieldset>
             <legend> رنگ و سایزبندی </legend>
             <div className="sizes">
               <label htmlFor="sizes"> سایز های موجود محصول </label>
-              <label htmlFor="siezSM"> SM </label><input type="checkbox" name="sizeSM" id="sizeSM" />
-              <label htmlFor="sizeS"> S </label><input type="checkbox" name="sizeS" id="sizeS" />
-              <label htmlFor="sizeM"> M </label><input type="checkbox" name="sizeM" id="sizeM" />
-              <label htmlFor="sizeL"> L </label><input type="checkbox" name="sizeL" id="sizeL" />
-              <label htmlFor="sizeXL"> XL </label><input type="checkbox" name="sizeXL" id="sizeXL" />
+              <label htmlFor="siezSM"> SM </label>
+              <input type="checkbox" name="sizeSM" id="sizeSM" />
+              <label htmlFor="sizeS"> S </label>
+              <input type="checkbox" name="sizeS" id="sizeS" />
+              <label htmlFor="sizeM"> M </label>
+              <input type="checkbox" name="sizeM" id="sizeM" />
+              <label htmlFor="sizeL"> L </label>
+              <input type="checkbox" name="sizeL" id="sizeL" />
+              <label htmlFor="sizeXL"> XL </label>
+              <input type="checkbox" name="sizeXL" id="sizeXL" />
             </div>
             <div className="colors">
               <label htmlFor="colors"> رنگ های موجود محصول </label>
-              <label htmlFor="colorB"> Blue </label><input type="checkbox" name="colorB" id="colorB" />
-              <label htmlFor="colorBe"> Beige </label><input type="checkbox" name="colorBe" id="colorBe" />
-              <label htmlFor="colorR"> Red </label><input type="checkbox" name="colorR" id="colorR" />
-              <label htmlFor="colorG"> Green </label><input type="checkbox" name="colorG" id="colorG" />
-              <label htmlFor="colorP"> Pink </label><input type="checkbox" name="colorP" id="colorP" />
+              <label htmlFor="colorB"> Blue </label>
+              <input type="checkbox" name="colorB" id="colorB" />
+              <label htmlFor="colorBe"> Beige </label>
+              <input type="checkbox" name="colorBe" id="colorBe" />
+              <label htmlFor="colorR"> Red </label>
+              <input type="checkbox" name="colorR" id="colorR" />
+              <label htmlFor="colorG"> Green </label>
+              <input type="checkbox" name="colorG" id="colorG" />
+              <label htmlFor="colorP"> Pink </label>
+              <input type="checkbox" name="colorP" id="colorP" />
             </div>
           </fieldset>
           <fieldset>
             <legend> قیمت و تخفیف های محصول </legend>
-            <input className="newPrInput" name='originalPrice' type="text" placeholder='قیمت اصلی محصول' />
-            <input className="newPrInput" name='offerPrice' type="text" placeholder='قیمت بعد از تخفیف' />
-            <input className="newPrInput" name='percentage' type="text" placeholder='درصد تخفیف' />
+            <input
+              className="newPrInput"
+              name="originalPrice"
+              type="text"
+              placeholder="قیمت اصلی محصول"
+            />
+            <input
+              className="newPrInput"
+              name="offerPrice"
+              type="text"
+              placeholder="قیمت بعد از تخفیف"
+            />
+            <input
+              className="newPrInput"
+              name="percentage"
+              type="text"
+              placeholder="درصد تخفیف"
+            />
           </fieldset>
         </section>
-        <section className='leftSide'>
+        <section className="leftSide">
           <fieldset>
             <legend> عکس های مصحول شما </legend>
             <input
@@ -124,8 +215,8 @@ export default function NewProduct() {
             {imagePreview && <img src={imagePreview} alt="Preview" />}
           </fieldset>
         </section>
-        <button type='submit' disabled={isLoading}>
-          {isLoading ? 'در حال ایجاد...' : 'ایجاد محصول'}
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? "در حال ایجاد..." : "ایجاد محصول"}
         </button>
         {isError && <div className="error-message">{error.message}</div>}
       </form>
